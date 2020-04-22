af8b47e3f958916db6bf6a3e1a3a46d4ffe7abfc
Sai-aws-repo <saikumar.devabaktuni@gmail.com>
2020-04-22 11:23:57 -0500

    first steps

---
+++ 01/vars.tf
@@ -0,0 +1,16 @@
+variable "AWS_ACCESS_KEY" {}
+
+variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {
+  default = "us-east-2"
+}
+
+variable "AMIS" {
+  type = "map"
+  default = {
+    us-east-2 = "ami-0f7919c33c90f5b58"
+    us-east-1 = "ami-0323c3dd2da7fb37d"
+  }
+}
+
